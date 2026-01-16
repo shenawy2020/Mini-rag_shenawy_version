d0cf5faffb83515af6cb633632d5ad7a1b884017
Mohammed Elshenawy <MElshenawy@healthinsightsgroup.com>
2026-01-17 00:57:34 +0200

    Create vector db schema

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from routes import base,data
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
+from stores.llm import LLMProviderFactory
+
 
 app = FastAPI()
 
@@ -12,11 +14,21 @@ async def startup_db_client():
     #print (settting.MONOGDB_Database)
     app.mogon_conn= AsyncIOMotorClient(settting.MONOGDB_ULR)
     app.dbclient=app.mogon_conn[settting.MONOGDB_Database]
+    #Initialize LLM Provider Factory
+    app.Genration_client=LLMProviderFactory(conifig=settting).create_llm_provider(provider=settting.GENERATION_BACKEND)
+    app.embedding_client=LLMProviderFactory(conifig=settting).create_llm_provider(provider=settting.EMBEDDING_BACKEND)
+    app.Genration_client.set_generatuion_model(mode_id=settting.GENERATION_MODEL_ID)
+    #app.Genration_client.set_embedding_model(model_id=settting.EMBEDDING_MODEL_ID,embedding_size=settting.EMBEDDING_MODEL_SIZE) 
+
 
 @app.on_event("shutdown")
 async def shutdown_db_client():
       app.dbclient.close()
 
+
+#app.rooter.lifespnan.on_startup.append(startup_db_client)   
+
+#app.rooter.lifespnan.on_shutdown.append(shutdown_db_client)
 
 
 app.include_router(base.base_router)
