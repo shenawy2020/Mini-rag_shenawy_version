30e14b6fb44f9a9be01b57778d87d0cc1256faca
Mohammed Elshenawy <MElshenawy@healthinsightsgroup.com>
2026-01-17 00:57:02 +0200

    add llm settting

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -4,16 +4,35 @@ class Settings(BaseSettings):
 
     APP_NAME: str
     APP_VERSION:  str
-    OPENAI_API_KEY: str
-    Cohere_API_KEY: str
+     
+    
     FILE_MAX_SIZE:int
     FILE_ALLOWED_TYPES:list
     FILE_DEFAULT_CHUNK_SIZE: int
     MONOGDB_ULR:str
     MONOGDB_Database:str
     FILE_ALLOWED_EXT:list[str]
+    GENERATION_BACKEND : str 
 
-    
+    EMBEDDING_BACKEND: str 
+
+    OPENAI_API_KEY:str=None
+    OPENAI_API_URL:str=None
+
+    COHERE_API_KEY:str=None
+
+    GENERATION_MODEL_ID:str=None
+    EMBEDDING_MODEL_ID:str=None
+    EMBEDDING_MODEL_SIZE:int=None
+
+    INPUT_DAFAULT_MAX_CHARACTERS:int=None
+    GENERATION_DAFAULT_MAX_TOKENS:int=None
+    GENERATION_DAFAULT_TEMPERATURE:float  =None
+      
+
+
+   
+
     class config:
        env_file = ".env"
        SettingsConfigDict
